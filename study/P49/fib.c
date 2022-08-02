f9c1d93fb2d9235ea61d9b2c2a665cebf551caf4
chang-jing-jing <2249077862@qq.com>
2022-08-02 12:04:55 +0800

    22.8.2

--- study/P49/fib.c
+++ study/P49/fib.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int fib(int num) {
-    if (num == 1 || num == 2)
-        return 1;
-    else
-        return fib(num - 1) + fib(num - 2);
-}
+// int fib(int num) {
+//     if (num <= 2)
+//         return 1;
+//     else
+//         return fib(num - 1) + fib(num - 2);
+// }
 
-int main() {
-    int num = fib(9);
+// int main() {
+//     int num = fib(10);
 
-    printf("%d\n", num);
+//     printf("%d\n", num);
 
-    return 0;
-}
+//     return 0;
+// }
