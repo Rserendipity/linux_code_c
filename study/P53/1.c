71701e1f0e5ce925ec063ac95c1f08f3ca4d50f6
chang-jing-jing <2249077862@qq.com>
2022-08-03 12:42:19 +0800

    22.8.3

--- study/P53/1.c
+++ study/P53/1.c
@@ -1,8 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
 
+// void swap(char* a, char* b) {
+//     char temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
 
-int main() {
+// void reverStr(char* s, int head, int tail) {
+//     if (head < tail) {
+//         swap(&s[head], &s[tail]);
+//     } else {
+//         reverStr(s, head + 1, tail - 1);
+//     }
+// }
+
+// int main() {
+//     char arr[] = "abcdef";
+//     int size = strlen(arr);
+
+//     printf("%s\n", arr);
+
+//     reverStr(arr, 0, size - 1);
     
-    return 0;
-}
+//     printf("%s\n", arr);
+
+//     return 0;
+// }
