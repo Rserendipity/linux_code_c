87840cad7d7d85c0ab428df4c6a85e45b9adfc78
changjingjing <2249077862@qq.com>
2022-08-11 19:26:44 +0800

    优化App,接近完成

--- App/1.c
+++ App/1.c
@@ -11,6 +11,10 @@ int main() {
     FILE* pf_en = fopen("./en.txt", "r");
     FILE* pf_ch = fopen("./ch.txt", "r");
     FILE* pf_mem = fopen("./info.txt", "r");
+    if (pf_en == NULL || pf_ch == NULL || pf_mem == NULL) {
+        printf("必要文件信息丢失或已被修改！\n");
+        return -1;
+    }
 
     // 记录当前定位的位置信息,方便下次打开的时候直接读取
     // 相当于是保存记录
@@ -18,24 +22,29 @@ int main() {
     unsigned long long seck_ch = 0;
 
     // 各自缓冲区
-    char en_buffer[30] = {0};
-    char ch_buffer[100] = {0};
+    char en_buffer[50] = {0};
+    char ch_buffer[300] = {0};
 
     // 偏移文件指针,即恢复记录
     fscanf(pf_mem, "%lld %lld", &seck_en, &seck_ch);
     fseek(pf_ch, seck_ch, 0);
     fseek(pf_en, seck_en, 0);
+    // 改变偏移量以后就可以释放掉当前的info.txt文件了，释放资源
+    fclose(pf_mem);
 
     char ch = 0;
     for (int j = 0; j < 5; j++) {
         for (int i = 0; (ch = fgetc(pf_en)) != '\n'; i++) {
             en_buffer[i] = ch;
+            seck_en++;
         }
         for (int i = 0; (ch = fgetc(pf_ch)) != '\n'; i++) {
+            // 读取到字符且不是第一个元素且前面一个元素不是字符时进行换行
             if (i > 0 && isword(ch) && !isword(ch_buffer[i - 1])) {
                 ch_buffer[i++] = '\n';
             }
             ch_buffer[i] = ch;
+            seck_ch++;
         }
         printf("%s\n", en_buffer);
         printf("%s\n", ch_buffer);
@@ -43,9 +52,11 @@ int main() {
         memset(ch_buffer, 0, 100);
     }
 
+    // 把当前的字节偏移信息写入到文件，方便下次获取
+    fprintf(pf_mem, "%lld %lld", seck_en, seck_ch);
+
     // 释放文件流
     fclose(pf_en);
     fclose(pf_ch);
-    fclose(pf_mem);
     return 0;
 }
