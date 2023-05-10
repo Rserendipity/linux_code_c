8e66247cf92323eeb0b8fc63d10093eddd0ee811
cjj <2249077862@qq.com>
2023-05-10 16:37:34 +0800

    删除原来文件

--- App/背单词/2.0/源码/Model.h
+++
@@ -1,4 +0,0 @@
-#include "Word.h"
-extern void SelectModel(Word* word);
-extern void InputModel(Word* word);
-extern void ClearRecord(Word* word);
--- bit/bilibili/P59/2.c
+++
@@ -1,9 +0,0 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-
-// int main() {
-//     float a = 1 / 3.0f;
-//     printf("%f", a);
-//     return 0;
-// // }
